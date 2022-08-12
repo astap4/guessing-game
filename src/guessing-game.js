9291b1e243b3082d176a9e79b7bc2479f9c84f38
Alina <astapchik.1993@mail.ru>
2022-08-12 20:47:16 +0300

    feat: guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,42 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
+
 
     guess() {
-
+        this.guessingNumber = Math.round((this.min + this.max) / 2);
+        console.log("Guess number is: " + this.guessingNumber)
+        return this.guessingNumber
+        // if (this.guessingNumber == this.number) {
+        //     console.log("You won!")
+        //     return this.guessingNumber;}
+        // else if (this.guessingNumber > this.number) {
+        //     console.log("The number should be lower")
+        // }
+        // else if (this.guessingNumber < this.number) {
+        //     console.log("The number should be greater")
+        // }
     }
+
 
     lower() {
-
+        this.max = this.guessingNumber;
+        console.log("Now max is: " + this.max)
     }
 
-    greater() {
 
+    greater() {
+        this.min = this.guessingNumber;
+        console.log("Now min is: " + this.min)
     }
 }
 
 module.exports = GuessingGame;
+
+
